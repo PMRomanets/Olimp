32c81c2fc23489139e16e65902d613e4a5de9689
Petro Romanets <pn_romanets@yahoo.com>
2020-08-30 07:57:30 +0300

    flask_app.py (old nsame main.py) all app init must be under the if __name__ == '__main__'git add .!

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,15 @@
-import pandas as pd
-from configs.config import parameter
-from os.path import join
-from flask import Flask
-from app.scedules_app import Schedules
-from app.interesting_app import InterestingClass
-from app.home_page import get_app
 from flask_login import current_user
-from app.contacts_app import ContactsClass
-from app.staff_only_login import LoginProcess, User
 from flask import Flask, Response
 from flask_login import LoginManager, UserMixin, login_required
 from flask import Response, request
 from app.user import get_login_form_html
 from os.path import join as path_join
+from configs.config import parameter
+from app.scedules_app import Schedules
+from app.interesting_app import InterestingClass
+from app.home_page import get_app
+from app.contacts_app import ContactsClass
+from app.staff_only_login import LoginProcess, User
 
 
 app = Flask(__name__)
@@ -74,28 +71,29 @@ def fchief():
     print(current_user.name)
     return app_home.index()
 
+
+asset_dir = parameter["assets_dir"]
+dummy_text = "СТОРІНКА ЗНАХОДИТСЯ НА СТАДІЇ РОЗРОБКИ"
+many_thanks_text = "ДЯКУЄМО ЩО СКОРИСТАЛИСЯ СЕРВІСОМ!"
+obj_schedule = Schedules("/", "НА ГОЛОВНУ")
+schedules = obj_schedule.get_app(app, "/schedules/")
+dir_ = path_join(asset_dir,"interesting_K")
+interesting_k_obj = InterestingClass("/", "Цікавинки карате", dir_, fig_ext="jpeg", limit_objects_number=3)
+interesting_k_app = interesting_k_obj.get_app(app, "/interesting_K/")
+dir_ = path_join(asset_dir, "interesting_Y")
+interesting_y_obj = InterestingClass("/", "Цікавинки йоги", dir_, fig_ext="jpeg", limit_objects_number=3)
+interesting_y_app = interesting_y_obj.get_app(app, "/interesting_Y/")
+dir_ = path_join(asset_dir, "interesting_D")
+interesting_d_obj = InterestingClass("/", "Цікавинки у світі танцю", dir_, fig_ext="jpeg", limit_objects_number=1)
+interesting_d_app = interesting_d_obj.get_app(app, "/interesting_D/")
+
+dir_ = path_join(asset_dir, "summertime")
+interesting_d_obj = InterestingClass("/", "Цікавинки у світі танцю", dir_, fig_ext="jpeg", limit_objects_number=1)
+interesting_d_app = interesting_d_obj.get_app(app, "/summertime/")
+
+contacts_obj = ContactsClass("/", "Вдячні за Вашу цікавіть до клубу! Зв'яжіться з нами!")
+contacts_app = contacts_obj.get_app(app, "/contacts/")
+# obj_main = Schedules(f"/home/", "НА ГОЛОВНУ")
+app_home = get_app(app, f"/")
 if __name__ == '__main__':
-    asset_dir = parameter["assets_dir"]
-    dummy_text = "СТОРІНКА ЗНАХОДИТСЯ НА СТАДІЇ РОЗРОБКИ"
-    many_thanks_text = "ДЯКУЄМО ЩО СКОРИСТАЛИСЯ СЕРВІСОМ!"
-    obj_schedule = Schedules("/", "НА ГОЛОВНУ")
-    schedules = obj_schedule.get_app(app, "/schedules/")
-    dir_ = path_join(asset_dir,"interesting_K")
-    interesting_k_obj = InterestingClass("/", "Цікавинки карате", dir_, fig_ext="jpeg", limit_objects_number=3)
-    interesting_k_app = interesting_k_obj.get_app(app, "/interesting_K/")
-    dir_ = path_join(asset_dir, "interesting_Y")
-    interesting_y_obj = InterestingClass("/", "Цікавинки йоги", dir_, fig_ext="jpeg", limit_objects_number=3)
-    interesting_y_app = interesting_y_obj.get_app(app, "/interesting_Y/")
-    dir_ = path_join(asset_dir, "interesting_D")
-    interesting_d_obj = InterestingClass("/", "Цікавинки у світі танцю", dir_, fig_ext="jpeg", limit_objects_number=1)
-    interesting_d_app = interesting_d_obj.get_app(app, "/interesting_D/")
-
-    dir_ = path_join(asset_dir, "summertime")
-    interesting_d_obj = InterestingClass("/", "Цікавинки у світі танцю", dir_, fig_ext="jpeg", limit_objects_number=1)
-    interesting_d_app = interesting_d_obj.get_app(app, "/summertime/")
-
-    contacts_obj = ContactsClass("/", "Вдячні за Вашу цікавіть до клубу! Зв'яжіться з нами!")
-    contacts_app = contacts_obj.get_app(app, "/contacts/")
-    # obj_main = Schedules(f"/home/", "НА ГОЛОВНУ")
-    app_home = get_app(app, f"/")
     app.run(port=5500, debug=True)
